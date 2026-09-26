b85c3c6e7885f6b0eb36f448378b53b3a8df7d85
Hawas backer <hawas.backer@vonnue.com>
2026-09-26 13:59:37 +0530

    feat(tasks): add task page route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import DashboardPage from './pages/DashboardPage';
+import TaskPage from './pages/TaskPage';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/" element={<DashboardPage />} />
+      <Route path="/tasks/:taskId" element={<TaskPage />}></Route>
     </Routes>
   );
 }
